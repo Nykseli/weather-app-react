c3741d32a6df2467f81a85a9d16a9ecefe8cc7d5
Miika Alikirri <miika.alik@gmail.com>
2019-09-16 21:26:10 +0300

    You can now search the city on the front page if you are logged in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
+import CookieService from './services/CookieService'
 import './App.css';
 
 function App() {
+  // Make sure that the user is logged in
+  const apiToken = CookieService.getCookie('api_token')
+  if(!apiToken) {
+    window.location = 'http://localhost:3000/login'
+  }
+
+  const [cityName, setCityName] = useState('')
+
+  const searchWeather = (event) => {
+    event.preventDefault()
+    window.location = `http://localhost:3000/city/${cityName}`
+    return false
+  }
+
+  const cityNameChange = (event) => {
+    setCityName(event.target.value.toString().toLowerCase())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-div">
+      <form onSubmit={searchWeather}>
+        <div className="container">
+          <label for="uname"><b>Search city</b></label>
+          <input onChange={cityNameChange} type="text" placeholder="Search city" name="uname" required />
+          <button type="submit">Search</button>
+        </div>
+      </form>
     </div>
   );
 }
